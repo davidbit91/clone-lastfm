0e4047bd200a6a9ed33c356d65faf51b26435316
davidbit91 <david.garcia@student.bit.es>
2020-11-02 14:04:25 +0100

    1rst

--- script.js
+++ script.js
@@ -1,36 +1,67 @@
 class Song {
-    constructor(){
+    constructor(name, duration, listeners, mbid, url, artist, attr, genre) {
+        this.name = name;
+        this.duration = duration;
+        this.listeners = listeners;
+        this.mbid = mbid;
+        this.url = url;
+        this.artist = artist;
+        this.attr = attr;
+        this.genre = genre;
     }
 
-    setItemLi(){
+    setItemLi() {
     }
-    setItemGroupName(group,url){
+    setItemGroupName(group, url) {
     }
-    setItemSongTitle(title){
+    setItemSongTitle(title) {
     }
-    setListeners(listeners){
+    setListeners(listeners) {
     }
-    getNewElement(group,url,title,listeners){
+    getNewElement(group, url, title, listeners) {
     }
 
 }
 
-const loadSongs = ()=>{
+const loadSongs = () => {
+    const cors = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": true
+        },
+        mode: 'no-cors',
+        cache: 'default'
+    };
 
-const loadOverview = () =>{
+    let songsJSON = "https://raw.githubusercontent.com/davidbit91/clone-lastfm/master/music.json";
+
+
+    let connect = fetch(songsJSON);
+
+    connect.then(val =>
+        val.json()
+    ).then(songs => {
+        console.log(songs);
+    });
+
+    
 }
 
-const loadTenListened = ()=>{
- 
+const loadOverview = () => {
 }
 
-const loadBiggest = (e)=>{
-   
+const loadTenListened = () => {
+
 }
 
-const init = ()=>{
- 
-    }
+const loadBiggest = (e) => {
+
+}
+
+const init = () => {
+    loadSongs();
+}
 
 
 window.onload = init;
